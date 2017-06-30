e3685593e1364fd55e1bd1080086f361c945700b
syoya <syoya@192.168.11.4>
2017-06-30 19:28:27 +0900

    aa

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/test.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/test.java
@@ -2,7 +2,7 @@ package jp.ac.meisei.j155.TheDarknessIsDeep;
 public class test {
 	public static void main(String[] args) {
 		//文字化けした？   mojibake -> chenge to UTF-8
-		//
+
 		System.out.println("Hello World!!");
 	}
 }
