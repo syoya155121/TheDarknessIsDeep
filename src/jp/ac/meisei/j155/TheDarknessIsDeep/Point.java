539c05f9684e7ebee120f0cbbf19e2941026df1a
syoya155121 <15j5121@stu.meisei-u.ac.jp>
2017-07-02 23:42:21 +0900

    Update Point.java
    コメント記入

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/Point.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Point.java
@@ -1,17 +1,20 @@
 package jp.ac.meisei.j155.TheDarknessIsDeep;
 public class Point {
+	//xは横 yは縦
 	private int x,y;
 
 	Point(int x,int y){
 		this.x=x;
 		this.y=y;
 	}
+	//x y をもらう系
 	public int getX(){
 		return x;
 	}
 	public int getY(){
 		return y;
 	}
+	//x y をセットする系
 	public void setPont(int x,int y){
 		this.x=x;
 		this.y=y;
