15506a88a78d225f9a5cee00d656fd6d2d6b181b
syoya <syoya@192.168.11.4>
2017-07-02 13:46:38 +0900

    ts

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/TdidMainGui.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/TdidMainGui.java
@@ -16,5 +16,6 @@ public class TdidMainGui extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		//aaa
 	}
 }
