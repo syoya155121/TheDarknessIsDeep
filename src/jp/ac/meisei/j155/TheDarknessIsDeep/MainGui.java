08729a38b2945b28ac0df986c96ae083b53c06d3
syoya <syoya@192.168.11.4>
2017-07-03 15:35:14 +0900

    修正

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/MainGui.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/MainGui.java
@@ -1,6 +1,8 @@
 package jp.ac.meisei.j155.TheDarknessIsDeep;
 import javax.swing.JFrame;
 
+import jp.ac.meisei.j155.TheDarknessIsDeep.GameState.States;
+
 public class MainGui extends JFrame {
 	GameState state = new GameState();
 	GuiTitle title = new GuiTitle(state);
@@ -10,10 +12,27 @@ public class MainGui extends JFrame {
 		setResizable(false);
 		getContentPane().add(title);
 		title.setVisible(true);
+		game.setVisible(false);
         this.setBounds(0,0,1000,750);
 	}
-	public void changePanel(int i){
-		title.setVisible(false);
+	public void changePanel(States state){
+		switch(state){
+		case Title:
+			title.setVisible(true);
+			break;
+		case Option:
+			title.setVisible(false);
+			break;
+		case Game:
+			title.setVisible(false);
+			break;
+		case TheEnd:
+			title.setVisible(false);
+			break;
+		case Result:
+			title.setVisible(false);
+			break;
+		}
 
 	}
 }
