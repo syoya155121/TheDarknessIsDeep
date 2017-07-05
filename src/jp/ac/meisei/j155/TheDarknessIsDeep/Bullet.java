fcd232687e03f367868a56bb7b34e7b841673c6c
takuji1231 <15j5056@stu.meisei-u.ac.jp>
2017-07-06 00:06:14 +0900

    fix@takuji

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/Bullet.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Bullet.java
@@ -3,12 +3,15 @@ package jp.ac.meisei.j155.TheDarknessIsDeep;
 import java.awt.*;
 
 public class Bullet{
-	Bullet(){
+	private int vx,vy; //弾速
+	Human[] bullet = new Human[5];
+	void pbullet(){
+		bullet[0] = new Human(".pic/bullet1.png",50,100);//プレーヤー弾
 	}
-	private int x,y,vx,vy;
-//プレーヤー弾　直線　弾=丸　一時的
-	public void paint(Graphics g){
-		g.setColor(Color.RED);
-		g.fillOval(x, y, 5, 5);
+	void ebullet1(){
+		bullet[1] = new Human(".pic/bullet2.png",800,200);//敵弾1
+	}
+	void ebullet2(){
+		bullet[2] = new Human(".pic/bullet3.png",800,400);//敵弾2
 	}
 }
