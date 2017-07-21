8c9e5f99932824658d4a0f549e67a28ba0cb179b
syoya <syoya@10.9.43.237>
2017-07-21 17:49:20 +0900

    fix

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/Boss.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Boss.java
@@ -1,7 +1,13 @@
 package jp.ac.meisei.j155.TheDarknessIsDeep;
 
+import java.awt.Graphics;
+
 public class Boss extends Human{
 	Boss(String file, int x, int y) {
 		super("./pic/boss.png",x,y);
+	}
+
+	public void moveBoss(int time,Graphics g){
+		this.paintImg(g);
 	}
 }
