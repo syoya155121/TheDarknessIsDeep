96360ea3c7eb732fc128a1d38fe6ddc2d25a151d
15j5056 <15j5056@WIN4753.stu.meisei-u.ac.jp>
2017-07-21 17:58:40 +0900

    fix

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/Player.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Player.java
@@ -1,7 +1,12 @@
 package jp.ac.meisei.j155.TheDarknessIsDeep;
 
+import java.awt.Graphics;
+
 public class Player extends Human{
 	Player(String file, int x, int y) {
 		super("./pic/player.png",x,y);
+	}
+	public void movePlayer(int time,Graphics g){
+		this.paintImg(g);
 	}
 }
