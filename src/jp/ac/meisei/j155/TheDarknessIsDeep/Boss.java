2efe16618ceae42e2b0891639f05dcbaea323581
syoya <syoya@10.9.43.237>
2017-07-21 15:57:17 +0900

    fix

---
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Boss.java
@@ -0,0 +1,7 @@
+package jp.ac.meisei.j155.TheDarknessIsDeep;
+
+public class Boss extends Human{
+	Boss(String file, int x, int y) {
+		super("./pic/boss.png",x,y);
+	}
+}
--- src/jp/ac/meisei/j155/TheDarknessIsDeep/Player.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Player.java
@@ -1,13 +1,5 @@
 package jp.ac.meisei.j155.TheDarknessIsDeep;
 
-//^^凸
-
-import java.awt.*;
-
-import javax.imageio.ImageIO;
-import java.awt.event.KeyEvent;
-import java.io.File;
-
 public class Player extends Human{
 	Player(String file, int x, int y) {
 		super("./pic/player.png",x,y);
