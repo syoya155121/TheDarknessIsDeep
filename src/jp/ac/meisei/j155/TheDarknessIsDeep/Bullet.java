fd1b087ab131b42e9b9bd3333c3c466a8b8b0645
takuji1231 <15j5056@stu.meisei-u.ac.jp>
2017-07-27 02:06:58 +0900

    fix@takuji

--- src/jp/ac/meisei/j155/TheDarknessIsDeep/Bullet.java
+++ src/jp/ac/meisei/j155/TheDarknessIsDeep/Bullet.java
@@ -2,18 +2,31 @@ package jp.ac.meisei.j155.TheDarknessIsDeep;
 
 import java.awt.*;
 
-public class Bullet{
-	private int vx,vy; //弾速
-	Human[] bullet = new Human[5];
-	void pbullet(){
-		bullet[0] = new Human(".pic/bullet1.png",50,100);//プレーヤー弾
+public class Bullet extends Human{
+	static int sx,sy;
+	Bullet(String file, int x, int y) {
+		super("./pic/bullet1.png", x, y);
+		sx=x;
+		sy=y;
 	}
-	void ebullet1(){
-		bullet[1] = new Human(".pic/bullet2.png",800,200);//敵弾1
+	public void moveBullet(int time,Graphics g){
+		this.paintImg(g);
+		x+=5;
 	}
-	void ebullet2(){
-		bullet[2] = new Human(".pic/bullet3.png",800,400);//敵弾2
+	public void init(){
+		this.x=sx;
+		this.y=sy;
 	}
+//	Human[] bullet = new Human[5];
+//	void pbullet(){
+//		bullet[0] = new Human(".pic/bullet1.png",50,100);//プレーヤー弾
+//	}
+//	void ebullet1(){
+//		bullet[1] = new Human(".pic/bullet2.png",800,200);//敵弾1
+//	}
+//	void ebullet2(){
+//		bullet[2] = new Human(".pic/bullet3.png",800,400);//敵弾2
+//	}
 }
 
 
